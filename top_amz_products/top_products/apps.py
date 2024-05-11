e8a45fe95ea29ea89468cdb705313dfb85ebceff
Michael Hy <hymike88@gmail.com>
2024-05-10 22:56:11 -0700

    adding comments on apps.py

--- top_amz_products/top_products/apps.py
+++ top_amz_products/top_products/apps.py
@@ -1,6 +1,22 @@
+"""
+django import from appconfig 
+"""
 from django.apps import AppConfig
 
 
 class TopProductsConfig(AppConfig):
+    """
+    Django application configuration for the 'top_products' app.
+
+    This AppConfig class defines configuration options for the 'top_products' app,
+    including the default auto-generated primary key field and the app name.
+
+    Attributes:
+        default_auto_field (str): The name of the default auto-generated primary key field.
+            This field is set to 'django.db.models.BigAutoField' to use a big integer field
+            as the primary key for models in the 'top_products' app.
+        name (str): The name of the Django app, which is set to 'top_products'.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "top_products"
