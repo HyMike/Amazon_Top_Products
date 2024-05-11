0de82c69a642221bae6afc8f370a099581e86d11
Michael Hy <hymike88@gmail.com>
2024-05-10 23:33:11 -0700

    added docstrings to the forms

--- top_amz_products/top_products/forms.py
+++ top_amz_products/top_products/forms.py
@@ -1,5 +1,19 @@
+"""
+Module for handling form-related functionality in Django.
+
+This module provides classes and utilities for working with forms in Django applications.
+"""
 from django import forms
 
 
 class CategoryForm(forms.Form):
+    """
+    A form for handling category data.
+
+    This form allows users to submit category URLs.
+
+    Attributes:
+        url (URLField): A URL field representing the category URL.
+            This field is rendered as a hidden input in the form.
+    """
     url = forms.URLField(widget=forms.HiddenInput())
