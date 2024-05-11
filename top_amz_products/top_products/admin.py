1cb3df1ee4f371198140714240f833fe20d9d841
Michael Hy <hymike88@gmail.com>
2024-05-10 22:31:59 -0700

    made changes to docstrings with urls.py and admin.py

--- top_amz_products/top_products/admin.py
+++ top_amz_products/top_products/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+"""
+Module for defining administration configurations.
+
+This module contains configurations for the Django admin interface.
+It provides registration for models to be managed via the admin site.
+
+"""
 
 # Register your models here.
--- top_amz_products/top_products/urls.py
+++ top_amz_products/top_products/urls.py
@@ -1,3 +1,10 @@
+"""
+URL configuration for the app.
+
+This module defines the URL patterns for the app. It contains mappings between
+URL paths and view functions or classes that handle HTTP requests.
+"""
+
 from django.urls import path
 from . import views
 
